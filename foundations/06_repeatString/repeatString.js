45982425d6f7880db5eb8a8ee96f8d3b1045e0f4
arrian <arrian.mor@gmail.com>
2026-06-15 12:33:47 -0400

    did second part

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -2,7 +2,11 @@ const repeatString = function(string, num) {
     if (num < 0) {
         return 'ERROR';
     }
-    
+    let string = '';
+    for (let i = 0; i < num; i++) {
+        string += string;
+    }
+    return string;
 };
 
 // Do not edit below this line
