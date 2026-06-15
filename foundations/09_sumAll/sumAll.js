58316e9f3ea0b3d7a40d7c781f0e48ef92b24de6
arrian <arrian.mor@gmail.com>
2026-06-15 12:56:33 -0400

    for loop, done!

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -6,6 +6,12 @@ const sumAll = function(min, max) {
         min = max;
         max = temp;
     }
+
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
     
 
     
