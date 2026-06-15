8dcb36ff8921c4e195e0f54602e7fce525cd45a8
arrian <arrian.mor@gmail.com>
2026-06-15 12:44:52 -0400

    for loop/done

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
 const removeFromArray = function() {
     let arr = arguments[0];
+    for (let i = 1; i < arguments.length; i++) {
+        arr = arr.filter(el => el !== arguments[i]);
+    }
+    return arr;
 
 
 };
