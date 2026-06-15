cd288d1200815b16e75bf5fdf2aa61280cca73c0
arrian <arrian.mor@gmail.com>
2026-06-15 13:14:41 -0400

    fourth condition + if statement hell

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -2,6 +2,18 @@ const leapYears = function(year) {
     const isALeapYear = year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
     const notALeapYear = year % 4 !== 0 || (year % 100 === 0 && year % 400 !== 0);
     const isCentury = year % 100 === 0;
+    const isDivisibleByFourHundred = year % 400 === 0;
+    
+    if (isALeapYear) {
+        return true;
+    } else if (notALeapYear) {
+        return false;
+    } else if (isCentury && !isDivisibleByFourHundred) {
+        return false;
+    } else if (isDivisibleByFourHundred) {
+        return true;
+    }
+
     
 
 
